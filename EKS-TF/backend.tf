85bdce0a2a0388ae64b5d663282accd1cb80602b
sheelykyle <102063425+sheelykyle@users.noreply.github.com>
2024-02-23 09:12:48 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "$(terraform output -raw bucket_name)"
+    bucket = ${terraform output -raw bucket_name}
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
