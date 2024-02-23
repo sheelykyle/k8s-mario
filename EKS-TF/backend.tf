0fa4ddf2082b604a996433aad8a35c99de0df168
sheelykyle <102063425+sheelykyle@users.noreply.github.com>
2024-02-22 22:29:38 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "aws_s3_bucket.mario-s3-bucket.id"
+    bucket = aws_s3_bucket.mario-s3-bucket.id
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
